cece5a03b0263c8b60a6c0bd4fcd0bd6111dc241
Craig Harman <craig@harmonicnewmedia.com>
2019-06-03 12:06:37 +0800

    Re-add password encryption

--- src/stubs/theme/User.php
+++ src/stubs/theme/User.php
@@ -40,7 +40,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
         'email_verified_at' => 'datetime',
     ];
 
-    // public function setPasswordAttribute($password) {
-    //     $this->attributes['password'] = Hash::make($password);
-    // }
+    public function setPasswordAttribute($password) {
+        $this->attributes['password'] = Hash::make($password);
+    }
 }
